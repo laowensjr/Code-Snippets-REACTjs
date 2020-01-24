e7fb33dedc2d5dba43a5a464f97452baf231605d
laowensjr <laowensjr@gmail.com>
2020-01-23 18:03:20 -0600

    Add files via upload

---
+++ index.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+class Car extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            brand: "Ford",
+            model: "Mustang",
+            color: "red",
+            year: 1964
+        };
+    }
+   changeColor = () => {
+        this.setState({ color: "blue" });
+    }
+    render() {
+        return (
+            <div>
+                <h1>My {this.state.brand}</h1>
+                <p>
+                    It is a {this.state.color} {this.state.model} from the year {this.state.year}.
+        </p>
+                <button
+                    type="button"
+                    onClick={this.changeColor}
+                >Change color</button>
+            </div>
+        );
+    }
+}
+
+
+ReactDOM.render(<Car />, document.getElementById('root'));
